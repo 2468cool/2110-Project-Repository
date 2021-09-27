0c3af8ad82a81c0503f2afee31648a9debede6b9
minec <minec@DESKTOP-1BO79OC>
2021-09-27 11:02:11 -0400

    Add project files.

---
+++ 2110-Project/Main.cpp
@@ -0,0 +1,44 @@
+//==========================================================
+//
+// Title:      CSC 2110 Project
+// Course:     CSC 2110
+// Project #:  Project 1
+// Authors:     <name>
+// Date:       <date>
+// Description:
+//   <brief description of application including its inputs,
+// processing, and outputs>
+//
+//==========================================================
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+using namespace std; // So "std::cout" may be abbreviated to "cout"
+
+int main()
+{
+
+	// Declare variables
+	//int i;
+	//double r;
+	//string s;
+	//char c;
+
+	// Show application header
+	cout << "Welcome to my Application!" << endl;
+	cout << "--------------------------" << endl << endl;
+
+	// Read from keyboard
+	//cout << "Enter a number: ";
+	//cin >> i;
+
+	// Write to screen
+	cout << "Hello World!" << endl;
+	cout << "Doing something useful here!" << endl;
+
+	// Show application close
+	cout << "\nEnd of my Application" << endl;
+
+}
