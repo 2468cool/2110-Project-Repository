95bf34b4f9f9c379480f1a23b0cf6701b5e6cbd8
Benjamin Pienta <hc7804@wayne.edu>
2021-09-27 11:09:37 -0400

    Intitial For Project. Linking to Repository

---
+++ 2110-Project/Account.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+using namespace std; // So "std::cout" may be abbreviated to "cout"
+
+class Account 
+{
+
+};
