69da625988ccc884008bcefb69252f15644f5923
Benjamin Pienta <hc7804@wayne.edu>
2021-09-29 09:58:13 -0400

    Added Client Class header

---
+++ 2110-Project/Client.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+using namespace std; // So "std::cout" may be abbreviated to "cout"
+
+class Client 
+{
+private:
+	int id;
+	string firstName;
+	string lastName;
+public:
+	// CheckingAccount checking;
+	// SavingAccount saving;
+	void setID(int i);
+	int getID() const;
+	void setName(string fn, string ln);
+	string getFirstName() const;
+	string getLastName() const;
+	Client(string i, string gn, string ln);
+};
