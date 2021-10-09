8af46278ef99cc067c3676e9d92a70d3d0a5e485
dreamzweaver <potatotomato@pm.me>
2021-10-09 12:17:20 -0400

    Added getters/settes to Client.cpp

--- 2110-Project/Client.cpp
+++ 2110-Project/Client.cpp
@@ -10,45 +10,38 @@ using namespace std; // So "std::cout" may be abbreviated to "cout"
 // Sets the id of the Client
 void Client::setID(int i)
 {
-
+	id = i;
 }
-
-/*
 // Returns the id of the client
 int Client::getID() const
 {
-	
+	return id;
 }
-*/
-
 // Sets the first and last name of the client
 void Client::setName(std::string fn, std::string ln)
 {
-
+	firstName = fn;
+	lastName = ln;
 }
-
-/*
 // Returns the first name of the client
 string Client::getFirstName() const
 {
-	
+	return firstName;
 }
-
 // returns the last name of the client
 string Client::getLastName() const
 {
-
+	return lastName;
 }
-*/
-
 //Default constructor to fix an error temporarily, unless this is the only way to make it work
 Client::Client()
 {
 
 }
-
 // Sets the id and name of client
 Client::Client(std::string i, std::string fn, std::string ln)
 {
-
+	id = stoi(i);
+	firstName = fn;
+	lastName = ln;
 }
